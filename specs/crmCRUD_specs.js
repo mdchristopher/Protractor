6ff32da28965dea03cd689ded9f78c7c41e71af4
Matthew Christopher <mchristopher@membersuite.com>
2016-04-29 15:39:38 -0400

    Edits to createAnIndividual Adding CRM CRUD spec

--- specs/crmCRUD_specs.js
+++ specs/crmCRUD_specs.js
@@ -2,9 +2,9 @@ var LoginPage = require('../pageObject/Common/loginPage.js');
 var ConsoleHomePage = require('../pageObject/Common/consoleHomePage.js');
 var OverviewPage = require('../pageObject/Common/overviewPage.js');
 var CreateAnIndividual = require('../pageObject/CRM/createAnIndividual.js');
-//var Individual360 = require('../pageObject/CRM/individual360');
+var Individual360 = require('../pageObject/CRM/individual360');
 
-describe('New Memberships', function () {
+describe('CRM CRUD', function () {
 
     beforeAll(function () {
         browser.ignoreSynchronization = true;
@@ -19,20 +19,37 @@ describe('New Memberships', function () {
         ch.clickSignOut()
     });
 
+    /*
     it('should create a new Individual', function () {
-
         var ch = new ConsoleHomePage();
-        ch.goToModule("Members");
-        expect(browser.getTitle()).toEqual('MemberSuite - CRM Overview');
-
+     ch.goToModule("CRM");
         var op = new OverviewPage();
         op.do('Create an Individual');
-        expect(browser.getTitle()).toEqual('MemberSuite - Create an Individual - Enter  Information');
-
         var ci = new CreateAnIndividual();
-        ci.enterBasicInfo("Matthew", "David", "Christopher", "mchristopher+auto@membersuite.com");
+     ci.enterBasicInfo("Matthew", "David", "Christopher", "mchristopher+auto@membersuite.com", "678-327-6801");
         ci.clickSave();
+     var ind360 = new Individual360();
+     ind360.waitForMessageBanner();
+    });
+     */
 
+    it('should create a new individual, edit, then save.', function () {
+        var ch = new ConsoleHomePage();
+        ch.goToModule("CRM");
+        var op = new OverviewPage();
+        op.do('Create an Individual');
+        var ci = new CreateAnIndividual();
+        ci.enterBasicInfo("Matthew", "David", "Christopher", "mchristopher+auto@membersuite.com", "678-327-6801");
+        ci.clickSave();
+        var ind360 = new Individual360();
+        ind360.waitForMessageBanner();
+        ind360.clickEditThisIndividual();
+        ci.enterBasicInfo("Matthew", "David", "Christopher", "mchristopher+auto@membersuite.com", "111-111-1111");
+        ci.clickSave();
+        ind360.waitForMessageBanner();
+
+        //expect(ind360.getPhoneNumberValue()).toContain("(111) 111-1111");
+        ind360.phoneNumberShouldEqual("(111) 111-1111");
     });
 
 });
